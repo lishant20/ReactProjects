2d1645c4cb5488ed233b2309d2599bf06c381c00
Lishant <lishantkarki@gmail.com>
2025-01-28 22:35:58 +0545

    Add Player page and routing; implement dynamic video fetching and navigation

--- NetFlix-Clone/src/pages/Player/Player.jsx
+++ NetFlix-Clone/src/pages/Player/Player.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png';
+import { useNavigate, useParams } from 'react-router-dom';
+import Home from '../Home/Home';
+
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTYxNzNlODI2MDM0MTFiMDBjMjcxZmU5NzZjNTcxZCIsIm5iZiI6MTczODA3NDI5MS41MzUsInN1YiI6IjY3OThlOGIzZGY0YjAxOTFjZTJiMmY4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eulYFZMH6TWIZ_ybVK0h2cFdUHdyiigpTWm6CHMGNWY'
+    }
+  };
+  
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results[0]))
+    .catch(err => console.error(err));
+  },[])
+  
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={() => {navigate(-2)}}/>
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
   )
 }
 
