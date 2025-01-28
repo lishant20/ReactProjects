f5b9f51eb28e04e6afd7be97ec6d7ef695aebebc
Lishant <lishantkarki@gmail.com>
2025-01-28 20:52:26 +0545

    Fetch movie data from API in TitleCards component; update Home page to use dynamic categories

--- NetFlix-Clone/src/components/TitleCards/TitleCards.jsx
+++ NetFlix-Clone/src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,19 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = ({title, category}) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTYxNzNlODI2MDM0MTFiMDBjMjcxZmU5NzZjNTcxZCIsIm5iZiI6MTczODA3NDI5MS41MzUsInN1YiI6IjY3OThlOGIzZGY0YjAxOTFjZTJiMmY4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eulYFZMH6TWIZ_ybVK0h2cFdUHdyiigpTWm6CHMGNWY'
+    }
+  };
 
   const handleWheel = (event) => {
     event.preventDefault(); //vertical 
@@ -12,6 +21,12 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category? category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel);
   },[])
   
@@ -19,10 +34,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className='card-list' ref={cardsRef}>
-        {cards_data.map((card,index) =>{
+        {apiData.map((card,index) =>{
           return <div className='card' key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
--- NetFlix-Clone/src/pages/Home/Home.jsx
+++ NetFlix-Clone/src/pages/Home/Home.jsx
@@ -27,10 +27,10 @@ const Home = () => {
         </div>
       </div>
       <div className='more-cards'>
-        <TitleCards title={"Blockbuster Movies"}/>
-        <TitleCards title={"Only on NetFlix"}/>
-        <TitleCards title={"Upcoming"}/>
-        <TitleCards title={"Top pics for you"}/>
+        <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>
+        <TitleCards title={"Only on NetFlix"} category={"popular"}/>
+        <TitleCards title={"Upcoming"} category={"upcoming"}/>
+        <TitleCards title={"Top pics for you"} category={"now_playing"}/>
       </div>
       <Footer />
     </div>
