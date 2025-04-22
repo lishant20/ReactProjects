8b595ab15ec1fbb8b923a6b99bf02c1969517b95
Lishant <lishantkarki@gmail.com>
2025-04-22 21:56:50 +0545

    Quizz app

---
+++ quizz/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "Which device is required for the Internet Connection?",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    ans: 1,
+  },
+  {
+    question: "Which continent has the highest number of countries?",
+    option1: "Asia",
+    option2: "Europe",
+    option3: "North America",
+    option4: "Africa",
+    ans: 4,
+  },
+  {
+    question: "Junk emails are also called?",
+    option1: "Spam",
+    option2: "Fake",
+    option3: "Archived",
+    option4: "Bin",
+    ans: 1,
+  },
+  {
+    question: "A computer cannot Boot if it doesnot have this?",
+    option1: "Application Software",
+    option2: "Internet",
+    option3: "Operating System",
+    option4: "Mouse",
+    ans: 3,
+  },
+  {
+    question: "First page of the website is also termed as?",
+    option1: "Index Page",
+    option2: "Home Page",
+    option3: "Site Map",
+    option4: "Pen Drive",
+    ans: 2,
+  }
+]
